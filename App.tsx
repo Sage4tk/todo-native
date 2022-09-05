df2a5745b1313dae672b4d59adcc08123422f173
timothy timbol <ttimbol08@gmail.com>
2022-09-05 23:24:57 +0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{counter}</Text>
+      <TouchableOpacity style={styles.btn} onPress={()=> setCounter(counter + 1)}>
+        <Text style={styles.btnColor}>Increase</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +20,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
   },
+  btn: {
+    backgroundColor: "#000",
+    paddingHorizontal: 20,
+    paddingVertical: 4,
+    borderRadius: 100,
+    marginTop:20,
+    width: 100
+  },
+  btnColor: {
+    color: "#fff",
+  }
+  
 });
